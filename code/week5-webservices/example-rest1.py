7d4f642076528129609932ad63c0ff58e8d08e04
Marc Verhagen <marc@cs.brandeis.edu>
2021-03-11 12:41:11 -0500

    Fixed error and made spme examples smoother.

--- code/week5-webservices/example-rest1.py
+++ code/week5-webservices/example-rest1.py
@@ -2,26 +2,20 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-message = "Hello \"John\" in the <PLACE>world</PLACE>!\n"
-
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/hello", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return jsonify({"message": "Hello"})
+    elif request.method == 'POST':
         some_json = request.get_json()
-        return jsonify({"you sent": some_json}), 201
-    else:
-        return jsonify({"about": message})
-
+        return jsonify({"message": some_json['message']}), 201
 
 @app.route("/multi/<int:num>", methods=['GET'])
 def multiply(num):
     return jsonify({"result": num * 10})
     
-    
 
 if __name__ == '__main__':
-
     app.run(debug=True)
 
 
@@ -29,6 +23,12 @@ if __name__ == '__main__':
 
 https://www.youtube.com/watch?v=s_ht4AKnWZg
 
-curl -v -H "Content-Type: application/json" -X POST -d '{"name": "John", "age": 10}' http://127.0.0.1:5000/
+$ curl http://127.0.0.1:5000/hello                # prints { "message": "Hello" }
+
+$ curl -v -H "Content-Type: application/json" -X POST -d '{"message": "Howdy"}' http://127.0.0.1:5000/hello
+
+$ curl http://127.0.0.1:5000/multi/10             # prints { "result": 100 }
+
+$ curl http://127.0.0.1:5000/multi/ten            # 404 URL not found error
 
 """
--- code/week5-webservices/example-rest2.py
+++ code/week5-webservices/example-rest2.py
@@ -1,24 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-message = "Hello <person>John</person>.\n"
-
-class HelloWorld(Resource):
+class Hello(Resource):
     def get(self):
-        return {"about": message}
+        name = request.args.get('name')
+        return {"hello": name}
     def post(self):
         some_json = request.get_json()
-        return {"you sent": some_json}, 201
+        return {"hello": some_json['name']}, 201
 
 class Multi(Resource):
-    def get(self):
+    def get(self, num):
         return jsonify({"result": num * 10})
-
-api.add_resource(HelloWorld, "/")
+    
+api.add_resource(Hello, "/hello")
 api.add_resource(Multi, "/multi/<int:num>")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -28,8 +28,14 @@ if __name__ == '__main__':
 
 https://www.youtube.com/watch?v=s_ht4AKnWZg
 
-$ curl http://127.0.0.1:5000
+$ curl http://127.0.0.1:5000/hello                # prints { "hello": null }
 
-$ curl -v -H "Content-Type: application/json" -X POST -d '{"name": "John", "age": 10}' http://127.0.0.1:5000/
+$ curl http://127.0.0.1:5000/hello?name=sue       # prints { "hello": "sue }
+
+$ curl -H "Content-Type: application/json" -X POST -d '{"name": "John"}' http://127.0.0.1:5000/hello
+
+$ curl http://127.0.0.1:5000/multi/10             # prints { "result": 100 }
+
+$ curl http://127.0.0.1:5000/multi/ten            # 404 URL not found error
 
 """
